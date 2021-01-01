4a1d797c70ee7c18fa62671aa56dfba31ef61f0a
phpunch <nutacp_ryho@hotmail.com>
2021-01-01 15:48:22 +0700

    Add log and env

---
+++ env/env.go
@@ -0,0 +1,16 @@
+package env
+
+// func init() {
+// 	// optionally look for config in the working directory
+// 	viper.AddConfigPath("./env/")
+// 	viper.SetConfigName("config")
+// 	viper.SetConfigType("json")
+// 	viper.ReadInConfig()
+// }
+
+// func GetEnv(envName string) string {
+// 	if viper.Get(envName) == nil {
+// 		return ""
+// 	}
+// 	return viper.Get(envName).(string)
+// }
