f07197522507dd7e826e43796ff383e04ed66e2d
phpunch <nutacp_ryho@hotmail.com>
2021-01-03 13:43:45 +0700

    Set foreign key

--- Docker/init.sql
+++ Docker/init.sql
@@ -1,13 +1,15 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email varchar(20),
+    email varchar(20) NOT NULL,
     password varchar(256),
     UNIQUE (email)
 );
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    user_id varchar(20),
+    user_id SERIAL NOT NULL,
     text varchar(256),
-    image_url text[]
+    image_url text[],
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
