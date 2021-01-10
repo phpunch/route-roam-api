fc3c5767974215a2055d770682a0a563264e4122
phpunch <nutacp_ryho@hotmail.com>
2021-01-10 15:29:03 +0700

    Add json tag

--- model/post.go
+++ model/post.go
@@ -1,9 +1,9 @@
 package model
 
 type Post struct {
-	ID       int64
-	UserID   int64
-	Text     string
-	ImageURL []string
-	LikedBy  []int
+	ID       int64    `json:"id"`
+	UserID   int64    `json:"user_id"`
+	Text     string   `json:"text"`
+	ImageURL []string `json:"image_url"`
+	LikedBy  []int    `json:"liked_by"`
 }
--- model/user.go
+++ model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	ID       int    `gorm:"primaryKey"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
